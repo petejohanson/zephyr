20840409e6e34a175f20f035bb52529bead00456
Peter Johanson <peter@peterjohanson.com>
2025-06-04 01:09:08 -0600

    cmake: modules: Add new post_boards_shields extension
    New post_boards_shields extension to customize files/config after
    boards and shields are fully resolved.
    
    Signed-off-by: Peter Johanson <peter@peterjohanson.com>

--- cmake/modules/zephyr_default.cmake
+++ cmake/modules/zephyr_default.cmake
@@ -94,6 +94,7 @@ list(APPEND zephyr_cmake_modules zephyr_module)
 
 list(APPEND zephyr_cmake_modules boards)
 list(APPEND zephyr_cmake_modules shields)
+list(APPEND zephyr_cmake_modules post_boards_shields)
 list(APPEND zephyr_cmake_modules snippets)
 list(APPEND zephyr_cmake_modules hwm_v2)
 list(APPEND zephyr_cmake_modules configuration_files)
